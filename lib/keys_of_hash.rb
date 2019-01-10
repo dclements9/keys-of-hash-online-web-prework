3b0ac33a3d282aa068ad9b6858b1820e07a55845
Dylan Clements <dclements@protonmail.com>
2019-01-10 16:56:00 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,7 @@
 class Hash
+  returned_array = []
   def keys_of(*arguments)
-    returned_array = []
+
         arguments.each do |arg_val|
             if key == arg_val
                 returned_array << key
